3f9c051aa9e7ad084604ce50bdea8315b8910ca3
Nikita Lyubimov <lyubimov@qtim.ru>
2023-09-29 11:01:21 +0300

    te

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import {Module, NestModule} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {StatiModule} from "./stati/stati.module";
@@ -6,14 +6,15 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {StatiController} from "./stati/controller/stati.controller";
 import {StatiService} from "./stati/services/stati.service";
 import {StatiEntity} from "./stati/entity/stati.entity";
-import {PrometheusModule} from "@willsoto/nestjs-prometheus";
-
+import {makeCounterProvider, PrometheusModule} from "@willsoto/nestjs-prometheus";
+import {ApiMetricsMiddleware} from "./api.metrics.middleware";
+import {collectDefaultMetrics} from "prom-client";
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       "type": "postgres",
-      "host": "172.16.238.16",
-      "port": 5432,
+      "host": "localhost",
+      "port": 5433,
       "username": "test",
       "password": "test",
       "database": "test",
@@ -21,9 +22,74 @@ import {PrometheusModule} from "@willsoto/nestjs-prometheus";
       "entities": [StatiEntity],
       "synchronize": true
     }),
-    PrometheusModule.register(),
-    StatiModule],
+    StatiModule,
+      PrometheusModule.register({
+          defaultMetrics: {
+              enabled: true,
+              // See https://github.com/siimon/prom-client#configuration
+              config: {},
+          },
+      }),
+
+    ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    makeCounterProvider(
+        {
+          name:'http_request_duration_milliseconds',
+          help:'http_request_duration_milliseconds_help',
+        },
+    ),
+    makeCounterProvider(
+        {
+          name:'http_request_size_bytes',
+          help:'http_request_size_bytes_help',
+        },
+    ),
+    makeCounterProvider(
+        {
+          name:'http_response_size_bytes',
+          help:'http_response_size_bytes_help',
+        },
+    ),
+    makeCounterProvider(
+        {
+          name:'http_all_request_total',
+          help:'http_all_request_total_help',
+        },
+    ),
+    makeCounterProvider(
+        {
+          name:'http_all_success_total',
+          help:'http_all_success_total_help',
+        },
+    ),
+    makeCounterProvider(
+        {
+          name:'http_all_errors_total',
+          help:'http_all_errors_total_help',
+        },
+    ),
+    makeCounterProvider(
+        {
+          name:'http_all_client_error_total',
+          help:'http_all_client_error_total_help',
+        },
+    ),
+    makeCounterProvider(
+        {
+          name:'http_all_server_error_total',
+          help:'http_all_server_error_total_help',
+        },
+    ),
+    makeCounterProvider(
+        {
+          name:'http_request_total',
+          help:'http_request_total_help',
+        },
+    ),
+
+      ApiMetricsMiddleware,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ApiMetricsMiddleware} from "./api.metrics.middleware";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(ApiMetricsMiddleware);
   await app.listen(3030);
 }
 bootstrap().then(r => console.log('start'));
