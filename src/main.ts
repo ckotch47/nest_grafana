72e49050f0be7af48b6040e891441eec56ed72ff
maxkiner <kirpichnikov@qtim.ru>
2023-10-02 16:47:52 +0300

    Fix middleware

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ApiMetricsMiddleware} from "./api.metrics.middleware";
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(ApiMetricsMiddleware);
   await app.listen(3030);
 }
 bootstrap().then(r => console.log('start'));
